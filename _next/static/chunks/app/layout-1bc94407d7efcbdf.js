db45ec4cddece520b97a280f017b245c9c625983
Nooruddin Shaikh <nooruddinsabeel@gmail.com>
2026-09-19 08:22:29 +0400

    Revise Keturah Resort project information and MEP responsibilities

---
+++ _next/static/chunks/app/layout-1bc94407d7efcbdf.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{4440:()=>{},7551:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4440,23))}},e=>{e.O(0,[549,569,470,358],()=>e(e.s=7551)),_N_E=e.O()}]);
